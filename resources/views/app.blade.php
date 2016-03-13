a5194c2f76672c74b3e515b6a9b55e58f2bbd87f
千葉 裕一 <ychiba@officeyuai.net>
2016-03-14 04:42:01 +0900

    1603140441 commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <title>サンプル蔵</title>
-    <link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet"> 
-    @if ( $viewinfo->workID == '1')     <!-- 一件表示のコードエリア    -->
+    <link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet"> @if ( $viewinfo->workID == '1')
+    <!-- 一件表示のコードエリア    -->
     <link href="{{asset('/assets/css/prism.css')}}" rel="stylesheet">
     <style type="text/css">
         div.codebox {
@@ -33,16 +33,17 @@
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    @if ( $viewinfo->workID == '0') <!-- 一覧表示ならactive/ not enabled-->
-                    <li class="active" enabled = "false"><a>一覧 <span class="sr-only">(current)</span></a></li>
-                    @else                           <!-- でなければ、not active/ enabled -->
+                    @if ( $viewinfo->workID == '0')
+                    <!-- 一覧表示ならactive/ not enabled-->
+                    <li class="active" enabled="false"><a>一覧 <span class="sr-only">(current)</span></a></li>
+                    @else
+                    <!-- でなければ、not active/ enabled -->
                     <li><a href="/codelive">一覧 <span class="sr-only">(current)</span></a></li>
-                    @endif
-                    @if ( $viewinfo->workID == '2')
-                    <li class="active" enabled = "false"><a>新規 <span class="sr-only">(current)</span></a></li>
-                    @else                                  
+                    @endif @if ( $viewinfo->workID == '2')
+                    <li class="active" enabled="false"><a>新規 <span class="sr-only">(current)</span></a></li>
+                    @else
                     <li><a href="/codelive/create">新規</a></li>
-                    @endif                    
+                    @endif
                     <!-- ダミー　-->
                     <li><a href="">About</a></li>
                     <li><a href="">Contact</a></li>
